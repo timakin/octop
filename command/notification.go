0c91a8f556283144fc41d2136f7558f0b7b71202
timakin <timaki.st@gmail.com>
2016-03-13 19:14:09 +0900

    [modify] tab

--- command/notification.go
+++ command/notification.go
@@ -12,12 +12,11 @@ import (
 func CmdNotification(c *cli.Context) {
 	notifications := client.GetNotifications()
 	for _, notification := range notifications {
-		color.Cyan("> > > > > > > > > > > > > >")
-		fmt.Print(*notification.Subject.Title)
+		color.Cyan("-------------------------------------------------")
 		fmt.Print("\n")
+		fmt.Print("Title: \t" + *notification.Subject.Title)
 		fmt.Print("\n")
-		fmt.Print(*notification.Subject.URL)
-		fmt.Print("\n")
+		fmt.Print("Url: \t" + *notification.Subject.URL)
 		fmt.Print("\n")
 	}
 }
